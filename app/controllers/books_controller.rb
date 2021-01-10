00b1d558556b7b6f64710fbdb539b686f7072f0a
EC2 Default User <ec2-user@ip-172-31-42-212.ec2.internal>
2021-01-10 00:34:19 +0000

    [Add]削除機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,40 +1,52 @@
 class BooksController < ApplicationController
-  def index
-    @books = Book.all
-    @book = Book.new
-  end
+	def top
+	end
+	
+	def index
+		@books = Book.all
+		@book = Book.new
+	end
 
-  def show
-    @book = Book.find(params[:id])
-  end
+	def show
+		@book = Book.find(params[:id])
+	end
 
-  def new
-  end
-  
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path(blog.id)
-  end
+	def new
+	end
+	
+	def create
+		@book = Book.new(book_params)
+		if @book.save
+			flash[:notice] = "successfully"
+			redirect_to books_path(@book)
+		else
+			render :index
+		end
+	end
 
-  def edit
-    @book = Book.find(params[:id])
-  end
-  
-  def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to books_path(book)
-  end
-  
-  def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
-  end
-  
-  private
-  def book_params
-    params.require(:book).permit(:title, :body)
-  end
+	def edit
+		@book = Book.find(params[:id])
+		
+	end
+	
+	def update
+		@book = Book.find(params[:id])
+		if @book.update(book_params)
+			flash[:notice] = "successfully"
+			redirect_to book_path(@book)
+		else
+			render :edit
+		end
+	end
+	
+	def destroy
+		book = Book.find(params[:id])
+		book.destroy
+		redirect_to books_path
+	end
+	
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
+	end
 end
